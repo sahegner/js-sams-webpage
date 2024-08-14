49fbb203ff038f107297e02ef873580043b76c0d
Ryan Appel <appel@fvtc.edu>
2024-08-14 12:19:46 -0500

    done with demo

--- site.js
+++ site.js
@@ -2,6 +2,5 @@
 const contact = document.querySelector('#contact')
 
 contact.addEventListener('mouseover', () => {
-	//contact.parentElement.style.backgroundColor = 'lightblue'
 	contact.closest('nav').style.backgroundColor = 'lightblue'
 })
